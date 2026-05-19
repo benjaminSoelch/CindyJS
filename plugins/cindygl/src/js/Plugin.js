004ec29cf454cc139ff7b151831971625e7ca710
benjaminSoelch <benjamin.soelch@tum.de>
2026-05-19 13:42:19 +0200

    remove unused function

--- plugins/cindygl/src/js/Plugin.js
+++ plugins/cindygl/src/js/Plugin.js
@@ -1,21 +1,3 @@
-function m4Mul(A,B){
-  // TODO? hard-code result
-  // TODO? internally store matrices as flat array in GPU order
-  let C=[
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,0]
-  ];
-  for(let i=0;i<4;i++){
-    for(let j=0;j<4;j++){
-      for(let k=0;k<4;k++){
-        C[i][j]+=A[i][k]*B[k][j];
-      }
-    }
-  }
-  return C;
-}
 function m4FlatTranspose(M) {
     return [M[0][0],M[1][0],M[2][0],M[3][0],M[0][1],M[1][1],M[2][1],M[3][1],
         M[0][2],M[1][2],M[2][2],M[3][2],M[0][3],M[1][3],M[2][3],M[3][3]];
